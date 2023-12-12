32ff8719ed67b6ee0996778438be120e7a426661
mzalan <marquetant.zalan@students.jedlik.eu>
2023-12-12 14:17:02 +0100

    main

--- bank_projekt/bank_projekt/Ugyfelek.cs
+++ bank_projekt/bank_projekt/Ugyfelek.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bank_projekt
+namespace Ugyfelek
 {
-    internal class Ugyfelek
+    public class Ugyfelek
     {
+        
     }
 }
--- bank_projekt/bank_projekt/main.cs
+++ bank_projekt/bank_projekt/main.cs
@@ -1,1 +1,14 @@
-﻿, 
+﻿using Ugyfelek;
+
+Console.WriteLine("Üdvözöljük!");
+
+string beirtPin;
+do
+{
+    Console.Write("PIN-kód: ");
+    beirtPin = Console.ReadLine();
+} while (!(beirtPin.Length == 4));
+
+
+
+
