39d7bf9aad0873a19045eb79ad7e6805e4d80d62
mzalan <marquetant.zalan@students.jedlik.eu>
2023-12-28 11:38:30 +0100

    pénzfelvétel, fájlírás

--- bank_projekt/bank_projekt/main.cs
+++ bank_projekt/bank_projekt/main.cs
@@ -40,13 +40,63 @@ if (valasztas == "1")
         Console.WriteLine("3. Készpénz befizetés");
         Console.WriteLine("4. Utalás\n");
         Console.Write("Menüpont: ");
-        Console.ReadLine();
+        string valasztas2 = Console.ReadLine();
+
+        if(valasztas2 == "1")
+        {
+
+        }
+        else if(valasztas2 == "2")
+        {
+            
+            string kiiras = "";
+            List<string> osszegMenu = new List<string> {
+                "5 000",
+                "10 000",
+                "19 000",
+                "25 000",
+                "50 000",
+                "100 000",
+                "150 000",
+                "Más összeg"};
+            int beker;
+            do
+            {
+                beker = Fgvk.ValasztoMenu(osszegMenu, "Kérjük válassza ki felvenni kívánt összeget (Ft): ");
+                if (beker >= 1 && beker <= 7)
+                {
+                    kiiras = "\n" + Fgvk.KeszpenzCalc(int.Parse(osszegMenu[beker - 1].Replace(" ", "")));
+                }
+                else if (beker == 8)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Az automata a következő bankjegyeket bocsátja ki: 1 000 Ft, 2 000 Ft, 5 000 Ft, 10 000 Ft, 20 000 Ft");
+                    Console.WriteLine("Maximum felvehető összeg: 150 000 Ft");
+                    string hibauzenet;
+                    int bekerOssz;
+                    do
+                    {
+                        Console.Write("Adja meg az összeget: ");
+                        bekerOssz = int.Parse(Console.ReadLine());
+                        hibauzenet = Fgvk.CheckOsszeg(bekerOssz);
+                        Console.WriteLine(hibauzenet);
+                    } while (hibauzenet != "");
+                    kiiras = Fgvk.KeszpenzCalc(bekerOssz);
+                }
+                Console.WriteLine(kiiras);
+            } while (beker > 8 || beker < 1);
+
+        }
+        else if(valasztas2 == "3")
+        {
+            Console.WriteLine();
+        }
     }
 }
 
 if (valasztas == "2")
 {
-
+    
 }
 
 
